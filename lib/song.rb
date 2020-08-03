78337261f8d378236e859693719b96f354a42fc3
Anya Brun <tsbrun@icloud.com>
2020-08-03 20:28:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -30,6 +32,7 @@ class Song
 
   def self.find_by_name(song)
     @@all.find {|matching_song| song == matching_song}
+    binding.pry
   end
 
   def self.find_or_create_by_name
